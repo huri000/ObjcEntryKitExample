d86790094e6935d0a26400269360870e0c40a90f
Daniel Huri <huri000@gmail.com>
2018-09-29 10:48:25 +0300

    Add CocoaPods support

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.0'
+
+target 'ObjcEntryKitExample' do
+  use_frameworks!
+  pod 'SwiftEntryKit', '0.8.0'
+end
---
+++ Pods/Target Support Files/Pods-ObjcEntryKitExample/Pods-ObjcEntryKitExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ObjcEntryKitExample : NSObject
+@end
+@implementation PodsDummy_Pods_ObjcEntryKitExample
+@end
