2736c61287584c1ad6f0d22f6f57f599b1159adb
Daniel Huri <huri000@gmail.com>
2018-09-29 09:33:36 +0300

    Initial Commit

---
+++ ObjcEntryKitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcEntryKitExample
+//
+//  Created by Daniel Huri on 9/29/18.
+//  Copyright © 2018 huri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcEntryKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjcEntryKitExample
+//
+//  Created by Daniel Huri on 9/29/18.
+//  Copyright © 2018 huri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
